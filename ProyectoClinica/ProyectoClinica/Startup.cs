1c409111bdcfbb8f359b837f192c6370a3bf9d52
Cosmo0004 <108553854+Cosmo0004@users.noreply.github.com>
2025-01-15 14:48:10 -0600

    Add project files.

---
+++ ProyectoClinica/ProyectoClinica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoClinica.Startup))]
+namespace ProyectoClinica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
