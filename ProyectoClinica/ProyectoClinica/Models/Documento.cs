233d6ded6092972a14ddc6b480fac2567504b2a5
Anderx08 <andeespi08@gmail.com>
2025-02-06 16:36:54 -0600

    Nuevas tablas(sin salud mental)

---
+++ ProyectoClinica/ProyectoClinica/Models/Documento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoClinica.Models
+{
+    public class Documento
+    {
+        [Key]
+        public int Id_Documento { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        public string Informacion { get; set; }
+
+        [Required]
+        public int Id_Empleado { get; set; }
+
+        [ForeignKey("Id_Empleado")]
+        public Empleado Empleado { get; set; }
+    }
+}
---
+++ ProyectoClinica/ProyectoClinica/Models/NotificacionEmpleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoClinica.Models
+{
+    public class NotificacionEmpleado
+    {
+        [Key]
+        public int Id_Notificacion { get; set; }
+
+        [Required]
+        public int Id_Empleado { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [ForeignKey("Id_Empleado")]
+        public Empleado Empleado { get; set; }
+    }
+}
