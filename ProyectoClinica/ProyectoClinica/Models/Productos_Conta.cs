f4938d902cc7b2b5c83f064d3fbb7d8b95fa8249
AndrewCS2004 <andrewcr72.o@gmail.com>
2025-02-12 14:49:01 -0600

    Agregacion de Modelos y Migracion de Activos Fijos en Contabilidad

---
+++ ProyectoClinica/ProyectoClinica/Models/Productos_Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoClinica.Models
+{
+    public class Productos_Conta
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_Producto { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Nombre_producto { get; set; }
+
+        [Required]
+        public DateTime Creacion_producto { get; set; }
+
+        public DateTime? Actualizacion_producto { get; set; }
+
+        public DateTime? Eliminacion_producto { get; set; }
+
+        [Required]
+        public bool Estatus_producto { get; set; }
+
+        [StringLength(255)]
+        public string Tipo_producto { get; set; }
+
+        [Required]
+        public int Id_Usuario { get; set; }
+    }
+}
