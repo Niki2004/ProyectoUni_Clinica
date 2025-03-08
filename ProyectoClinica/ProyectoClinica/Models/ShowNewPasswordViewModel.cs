ca30cd808efad7c7224375f661b926cf1119d6fc
AndrewCS2004 <andrewcr72.o@gmail.com>
2025-03-08 01:29:28 -0600

    Inserciones en Reseteo de Contraseña

---
+++ ProyectoClinica/ProyectoClinica/Models/ShowNewPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoClinica.Models
+{
+    public class ShowNewPasswordViewModel
+    {
+        public string Email { get; set; }
+        public string TemporaryPassword { get; set; }
+    }
+}
