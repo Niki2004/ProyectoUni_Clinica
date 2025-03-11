4a0db92f58237f5835298131320fc4e468eea08b
Anderx08 <andeespi08@gmail.com>
2025-03-11 16:35:39 -0600

    Historial de expedientes

---
+++ ProyectoClinica/ProyectoClinica/Controllers/ExpedientesController.cs
@@ -0,0 +1,28 @@
+﻿using ProyectoClinica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoClinica.Controllers
+{
+    public class ExpedientesController : Controller
+    {
+        //Conexión BD
+        private ApplicationDbContext BaseDatos = new ApplicationDbContext();
+        // GET: Expedientes
+
+        public ActionResult Expedientes()
+        {
+            return View();
+        }
+
+        public ActionResult VistaHistorial()
+        {
+            var citas = BaseDatos.Cita.ToList();
+            return View(citas);
+        }
+
+    }
+}
